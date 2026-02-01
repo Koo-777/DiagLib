7fd4e67884b727cb000ccb14c6830bdd980137e7
Koo-777 <koo7sty@gmail.com>
2026-02-01 17:51:58 +0900

    chore: Add root redirect to basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/diag-lib',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/diag-lib',
+        basePath: false,
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
